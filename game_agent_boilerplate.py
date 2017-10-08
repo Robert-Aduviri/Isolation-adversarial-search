a6cf9554ee5ccebf2309a7070402e930ad3e3095
Robert Alonso <robert.alonso.ac@gmail.com>
2017-10-08 01:38:55 -0500

    Minimax implementation

--- game_agent_boilerplate.py
+++ game_agent_boilerplate.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
 """Finish all TODO items in this file to complete the isolation project, then
 test your agent's strength against a set of known agents using tournament.py
 and include the results in your report.
@@ -10,14 +5,10 @@ and include the results in your report.
 import random
 
 
-# In[9]:
-
 class SearchTimeout(Exception):
     """Subclass base exception for code clarity. """
     pass
 
-
-# In[3]:
 
 def custom_score(game, player):
     """Calculate the heuristic value of a game state from the point of view
@@ -47,8 +38,6 @@ def custom_score(game, player):
     raise NotImplementedError
 
 
-# In[4]:
-
 def custom_score_2(game, player):
     """Calculate the heuristic value of a game state from the point of view
     of the given player.
@@ -75,8 +64,6 @@ def custom_score_2(game, player):
     raise NotImplementedError
 
 
-# In[5]:
-
 def custom_score_3(game, player):
     """Calculate the heuristic value of a game state from the point of view
     of the given player.
@@ -102,8 +89,6 @@ def custom_score_3(game, player):
     # TODO: finish this function!
     raise NotImplementedError
 
-
-# In[6]:
 
 class IsolationPlayer:
     """Base class for minimax and alphabeta agents -- this class is never
@@ -133,8 +118,6 @@ class IsolationPlayer:
         self.time_left = None
         self.TIMER_THRESHOLD = timeout
 
-
-# In[12]:
 
 class MinimaxPlayer(IsolationPlayer):
     """Game-playing agent that chooses a move using depth-limited minimax
@@ -229,14 +212,9 @@ class MinimaxPlayer(IsolationPlayer):
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
 
-        # TODO: Working on this function
-        legal_moves = game.get_legal_moves()
-        if not legal_moves:
-            return (-1, -1)
-        return legal_moves[0]
+        # TODO: finish this function!
+        raise NotImplementedError
 
-
-# In[8]:
 
 class AlphaBetaPlayer(IsolationPlayer):
     """Game-playing agent that chooses a move using iterative deepening minimax
@@ -329,46 +307,3 @@ class AlphaBetaPlayer(IsolationPlayer):
 
         # TODO: finish this function!
         raise NotImplementedError
-
-
-# ### Example
-
-# In[11]:
-
-# from isolation import Board
-# from sample_players import RandomPlayer
-# from sample_players import GreedyPlayer
-
-# # create an isolation board (by default 7x7)
-# player1 = RandomPlayer()
-# player2 = GreedyPlayer()
-# game = Board(player1, player2)
-
-# # place player 1 on the board at row 2, column 3, then place player 2 on
-# # the board at row 0, column 5; display the resulting board state.  Note
-# # that the .apply_move() method changes the calling object in-place.
-# game.apply_move((2, 3))
-# game.apply_move((0, 5))
-# print(game.to_string())
-
-# # players take turns moving on the board, so player1 should be next to move
-# assert(player1 == game.active_player)
-
-# # get a list of the legal moves available to the active player
-# print(game.get_legal_moves())
-
-# # get a successor of the current state by making a copy of the board and
-# # applying a move. Notice that this does NOT change the calling object
-# # (unlike .apply_move()).
-# new_game = game.forecast_move((1, 1))
-# assert(new_game.to_string() != game.to_string())
-# print("\nOld state:\n{}".format(game.to_string()))
-# print("\nNew state:\n{}".format(new_game.to_string()))
-
-# # play the remainder of the game automatically -- outcome can be "illegal
-# # move", "timeout", or "forfeit"
-# winner, history, outcome = game.play()
-# print("\nWinner: {}\nOutcome: {}".format(winner, outcome))
-# print(game.to_string())
-# print("Move history:\n{!s}".format(history))
-
